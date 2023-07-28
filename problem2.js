930fe8dab01a50b3c1125f0986f08ff10a6ddeec
Habibur Rahman <habib.swi16@gmail.com>
2023-07-28 23:05:11 +0600

    2

---
+++ problem2.js
@@ -0,0 +1,11 @@
+let math = 75.25;
+let biology = 65;
+let chemistry = 80;
+let physics = 35.44;
+let bangla = 99.5;
+
+let total = math + biology + chemistry + physics + bangla;
+
+let average = total / 5;
+
+console.log(average);
